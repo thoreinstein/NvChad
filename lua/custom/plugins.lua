6998aa123214eaf5d14e0cdcd8dc062022146eed
Jim Anders <719823+thoreinstein@users.noreply.github.com>
2024-01-17 09:04:30 -0500

    Forgot to call setup

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -53,6 +53,9 @@ local plugins = {
   {
     "alexghergh/nvim-tmux-navigation",
     lazy = false,
+    config = function()
+      require("nvim-tmux-navigation").setup()
+    end,
   },
 }
 return plugins
